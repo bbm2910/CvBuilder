05375b927f1a741a275b28219903eda3cce240d6
Bobby M <b.mastacan@gmail.com>
2023-12-06 15:19:51 +0000

    Add files via upload

---
+++ MyDetails.jsx
@@ -0,0 +1,10 @@
+const MyDetails = () => {
+	return (
+		<div>
+			<a href='https://github.com/bbm2910' target='_blank'>
+				<img src='./public/github.png' alt='' className='github-logo' />
+			</a>
+		</div>
+	);
+};
+export default MyDetails;
